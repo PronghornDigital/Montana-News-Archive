7c68e92b9ea1c6d6856c536680cf86e95ba59c53
David Souther <davidsouther@gmail.com>
2015-11-22 09:54:05 -0500

    Gulpfile makes builds *much* faster.

---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+'use strict';
+
+let gulp = require('gulp');
+
+const INDEX = 'src/client/index.jade';
+const SOURCES = 'src/**/*.ts';
+const TEST_SOURCES = 'src/**/*.{mock,spec}.ts';
+const MOCKS = 'src/**/*mock.ts';
+const TYPEDEPS = 'src/typings/**/*.d.ts';
+const COMPILED_TEST_SOURCES = [
+  'dist/**/*.spec.js',
+  '!dist/client/**/*.spec.js'
+];
+const CLIENT_ROOT = 'src/client';
+const TEMPLATES = CLIENT_ROOT + '/**/*.jade';
+const STYLES = CLIENT_ROOT + '/**/*.scss';
+const STATIC = 'dist/_static';
+
+const DEFAULT_TASKS = ['build', 'test', 'lint'];
+
+
+
+/**
+ * Typescript gets compiled from src/ to dist/
+ */
+let gts = require('gulp-typescript');
+let tsProject = gts.createProject('tsconfig.json');
+let ts = gts(tsProject);
+gulp.task('build:tsc', function() {
+  let destination = gulp.dest('dist');
+  return gulp.src([SOURCES]).pipe(ts).pipe(destination);
+});
+
+/**
+ * Bundle client app.
+ */
+let webpack = require('webpack-stream');
+gulp.task('bundle:client', ['build:tsc'], function() {
+  let STATIC_DEST = gulp.dest(STATIC);
+  const INDEX_ROOT = 'dist/client/index.js';
+  const WPOPTS = require('./webpack.config.js');
+  return gulp.src(INDEX_ROOT).pipe(webpack(WPOPTS)).pipe(STATIC_DEST);
+});
+
+/**
+ * Compile all templates
+ */
+let jade = require('gulp-jade');
+let cache = require('gulp-angular-templatecache');
+gulp.task('index', function() {
+  let STATIC_DEST = gulp.dest(STATIC);
+  return gulp.src([INDEX]).pipe(jade()).pipe(STATIC_DEST);
+});
+gulp.task('templates', function() {
+  const TEMPL_DEST = gulp.dest(STATIC);
+  const CACHE_OPTS = {
+    module: 'mtna.templates',
+    standalone: true,
+    transformUrl: _ => '/' + _
+  };
+  return gulp.src([TEMPLATES]).pipe(jade()).pipe(cache(CACHE_OPTS)).pipe(TEMPL_DEST);
+});
+
+/**
+ * Put together custom styles.
+ */
+let sass = require('gulp-sass');
+let concatcss = require('gulp-concat-css');
+gulp.task('styles', function() {
+  let STATIC_DEST = gulp.dest(STATIC);
+  return gulp.src([STYLES]).pipe(sass({outputStyle: 'compressed'})).pipe(concatcss('styles.css')).pipe(STATIC_DEST);
+});
+
+/**
+ * Lint checks all of our code.
+ */
+let tslintconfig = require('./tslint.json');
+let tslint = require('gulp-tslint');
+gulp.task('lint:tslint', function() {
+  return gulp.src([SOURCES, `!${TYPEDEPS}`, `!${MOCKS}`])
+      .pipe(tslint({configuration: tslintconfig}))
+      .pipe(tslint.report('prose'));
+});
+
+/**
+ * Mocha tests *.spec.js in dist.
+ */
+let mochaconfig = require('./mochaconfig.json');
+let mocha = require('gulp-mocha')(mochaconfig);
+gulp.task('test:mocha', ['build'], function() {
+  return gulp.src(COMPILED_TEST_SOURCES).pipe(mocha);
+});
+
+let Server = require('karma').Server;
+let karmaConfig = require('./karma.config.json');
+gulp.task('test:karma', ['test:mocha'], function(done) {
+  new Server(karmaConfig, done).start();
+});
+
+/**
+ * Some default tasks.
+ */
+const DEFAULT = ['default'];
+gulp.task('default', DEFAULT_TASKS);
+gulp.task('build', ['build:tsc', 'bundle:client', 'templates', 'styles', 'index']);
+gulp.task('lint', ['lint:tslint']);
+gulp.task('test', ['test:mocha', 'test:karma']);
+gulp.task('watch', DEFAULT, function() { gulp.watch([SOURCES], DEFAULT); });
+
+if (require.main === module) {
+  gulp.runTask('default');
+}
